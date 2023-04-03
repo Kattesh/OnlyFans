1e96abdd8b42a1ed4c8e6c51aa5d3f2693886e40
Kattesh <kate.sh@icloud.com>
2023-04-02 19:56:58 -0500

    new post page

--- app/index.js
+++ app/index.js
@@ -1,13 +1,15 @@
 import {FlatList, StyleSheet, View} from "react-native";
 import users from '../assets/data/users'
 import UserCard from "../src/components/UserCard";
+import {Link} from "expo-router";
 
 export default function Page() {
     return (
         <View style={styles.container}>
+            <Link href={'/newPost'}>New post</Link>
             <FlatList
                 data={users}
-                renderItem={({item}) => <UserCard user={item}/>}
+                renderItem={({ item }) => <UserCard user={item} />}
                 showsVerticalScrollIndicator={false}
             />
         </View>
